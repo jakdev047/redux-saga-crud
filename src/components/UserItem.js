6a1943871c16ca9f1d8f015ed3e07371742fd4a8
jakdev047 <jakfreelancer047@gmail.com>
2020-08-13 13:05:38 +0600

    UserItem class  Component add

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { Button,Modal,ModalHeader,ModalBody,ModalFooter,Form,FormGroup,Input,CustomInput } from 'reactstrap';
+import React, { Component,Fragment } from 'react';
+import { Button} from 'reactstrap';
 
-const UserItem = ({
-   user: { first_name,email,avatar,last_name}
-}) => {
-  return (
-   <tr>
-      <td>
-         <img src={avatar} alt={first_name}/>
-      </td>
-      <td>{first_name}</td>
-      <td>{last_name}</td>
-      <td>{email}</td>
-      <td className="user-updt">
-         <Button color="user-update-icon" title='edit'>Edit </Button> 
-         <Button color="user-update-icon" title='delete'>Delete</Button>
-      </td>
-   </tr>
-  )
-}
+class UserItem extends Component {
+   constructor(props) {
+      super();
+   }
+  render() {
+     const { first_name,email,avatar,last_name } = this.props.user
+    return (
+      <Fragment>
+         <tr>
+            <td>
+               <img src={avatar} alt={first_name}/>
+            </td>
+            <td>{first_name}</td>
+            <td>{last_name}</td>
+            <td>{email}</td>
+            <td className="user-updt">
+               <Button color="user-update-icon" title='edit'>Edit </Button> 
+               <Button color="user-update-icon" title='delete'>Delete</Button>
+            </td>
+         </tr>
+      </Fragment>
+    )
+  }
+};
 
 export default UserItem;
+
