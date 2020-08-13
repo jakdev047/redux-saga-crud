31b15f74773235068368e39d73f3001a2887649b
jakdev047 <jakfreelancer047@gmail.com>
2020-08-13 10:46:59 +0600

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// ss
+import './assets/css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome React</h2>
+    </div>
+  );
+}
+
+export default App;
